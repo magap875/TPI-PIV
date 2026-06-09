7549b440b80ef02b065da7c54aa57e3f2e222dfd
magap875 <135629818+magap875@users.noreply.github.com>
2026-06-09 08:53:58 -0300

    estructura base

---
+++ TPI-PIV/TPI-PIV/src/main/java/com/example/TPI_PIV/TpiPivApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPI_PIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiPivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiPivApplication.class, args);
+	}
+
+}
---
+++ TPI-PIV/TPI-PIV/src/test/java/com/example/TPI_PIV/TpiPivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TPI_PIV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiPivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
