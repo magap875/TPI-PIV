a71cc7c33af40c138ca9789084a2529a814c873c
Mariano <achilli.mela@gmail.com>
2026-06-20 15:18:50 -0300

    logica de rankings

---
+++ TPI-PIV/TPI-PIV/src/main/java/com/example/features/rankings/controllers/RankingController.java
@@ -0,0 +1,31 @@
+package com.example.features.rankings.controllers;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.features.rankings.dtos.RankingResponseDTO;
+import com.example.features.rankings.services.interfaces.IRankingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ranking")
+@AllArgsConstructor
+public class RankingController {
+
+    private final IRankingService rankingService;
+
+    @GetMapping("/global")
+    public ResponseEntity<List<RankingResponseDTO>> rankingGlobal() {
+        return ResponseEntity.ok(rankingService.rankingGlobal());
+    }
+
+    @GetMapping("/grupos/{grupoId}")
+    public ResponseEntity<List<RankingResponseDTO>> rankingGrupo(
+            @PathVariable Long grupoId
+    ) {
+        return ResponseEntity.ok(rankingService.rankingGrupo(grupoId));
+    }
+}
---
+++ TPI-PIV/TPI-PIV/src/main/java/com/example/features/rankings/services/interfaces/IRankingService.java
@@ -0,0 +1,12 @@
+package com.example.features.rankings.services.interfaces;
+
+import java.util.List;
+
+import com.example.features.rankings.dtos.RankingResponseDTO;
+
+public interface IRankingService {
+
+    List<RankingResponseDTO> rankingGlobal();
+
+    List<RankingResponseDTO> rankingGrupo(Long grupoId);
+}
